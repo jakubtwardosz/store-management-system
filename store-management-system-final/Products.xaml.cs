e9d594433e8c1321095fca9cdedede45266101c3
Jakub Twardosz <jtwardosz13@gmail.com>
2021-02-26 22:19:11 +0100

    Products service completed

--- store-management-system-final/Products.xaml.cs
+++ store-management-system-final/Products.xaml.cs
@@ -30,27 +30,73 @@ namespace store_management_system_final
 
         private void AddProduct(object sender, RoutedEventArgs e)
         {
-
+            if (ProductsService.TryAddProduct(ProductName.Text, ProductBrandId.Text,  ProductCategoryId.Text,  ProductPrice.Text,  ProductQuantity.Text))
+            {
+                MessageBox.Show("Added!");
+            }
+            else
+            {
+                MessageBox.Show("Product not added!");
+            }
         }
 
         private void productsGridDataSelectionChanged(object s, SelectionChangedEventArgs e)
         {
+            ProductsService.selected = GetSelectedDisplay(e);
+            ProductName.Text = ProductsService.selected?.ProductName;
+            ProductBrandId.Text = ProductsService.selected?.BrandId.ToString();
+            ProductCategoryId.Text = ProductsService.selected?.CategoryId.ToString();
+            ProductPrice.Text = ProductsService.selected?.ProductPrice.ToString();
+            ProductQuantity.Text = ProductsService.selected?.ProductQuantity.ToString();
 
         }
 
+        private static products_displayed GetSelectedDisplay(SelectionChangedEventArgs e)
+        {
+            return e.AddedItems.Count == 0
+                ? null
+                : e.AddedItems[0] as products_displayed;
+        }
+
+
         private void ReadProduct(object sender, RoutedEventArgs e)
         {
-
+            ProductsDataGrid.ItemsSource = ProductsService.GetProductToDisplay();
+            MessageBox.Show("Loaded!");
         }
 
         private void UpdateProduct(object sender, RoutedEventArgs e)
         {
 
+            if (ProductsService.selected == null)
+            {
+                NotSelectedMessage();
+                return;
+            }
+
+            ProductsService.UpdateProduct(ProductName.Text, ProductPrice.Text, ProductQuantity.Text);
+
+            MessageBox.Show("Updated!");
+
         }
 
         private void DeleteProduct(object sender, RoutedEventArgs e)
         {
+            if (ProductsService.selected == null)
+            {
+                NotSelectedMessage();
+                return;
+            }
+
+            products DeletedProduct = ProductsService.DeleteSelectedProduct();
+            MessageBox.Show($"Deleted! {DeletedProduct?.product_id} ");
 
         }
+
+        private static void NotSelectedMessage()
+        {
+            MessageBox.Show("You must select something!");
+        }
+
     }
 }
--- store-management-system-final/ProductsService.cs
+++ store-management-system-final/ProductsService.cs
@@ -15,7 +15,7 @@ namespace store_management_system_final
             StoreDBEntities db = new StoreDBEntities();
 
             var products = from p in db.products
-                           select new products_displayed
+                           select new products_displayed 
                            {
                                Id = p.product_id,
                                ProductName = p.product_name,
@@ -27,13 +27,102 @@ namespace store_management_system_final
 
             return products.ToList();
         }
+
+        public bool TryAddProduct(string ProductName, string ProductBrandId, string ProductCategoryId, string ProductPrice, string ProductQuantity)
+        {
+            StoreDBEntities db = new StoreDBEntities();
+
+            if (int.TryParse(ProductPrice, out int Price)
+                && int.TryParse(ProductQuantity, out int Quantity))
+            {
+
+                int? category_id = null;
+                if (int.TryParse(ProductCategoryId, out int categoryId) && db.category.Any(category => category.category_id == categoryId))
+                {
+                    category_id = categoryId;
+                }
+
+                int? brand_id = null;
+                if (int.TryParse(ProductBrandId, out int brandId) && db.brands.Any(brands => brands.brand_id == brandId))
+                {
+                    brand_id = brandId;
+                }
+
+                products productsObject =
+                    new products() 
+                    {
+                        product_name = ProductName,
+                        product_price = Price,
+                        product_quantity = Quantity,
+                        brand_id = brand_id,
+                        category_id = category_id
+                    };
+
+                db.products.Add(productsObject);
+                db.SaveChanges();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public products DeleteSelectedProduct()
+        {
+            StoreDBEntities db = new StoreDBEntities();
+
+            // Pobierz z bazy order_items razem z orderem
+
+            var filteredProduct = from p in db.products.Include(nameof(products.order_items))
+                                  where p.product_id == selected.Id
+                                  select p;
+
+            products toDelete = filteredProduct.FirstOrDefault();
+
+            if (toDelete == null)
+            {
+                return null;
+            }
+
+            db.products.Remove(toDelete);
+
+            db.SaveChanges();
+
+            return toDelete;
+        }
+
+        public products UpdateProduct(string ProductName, string ProductPrice, string ProductQuantity)
+        {
+            StoreDBEntities db = new StoreDBEntities();
+
+            var products = from p in db.products
+                           where p.product_id == selected.Id
+                           select p;
+
+            products toUpdate = products.FirstOrDefault();
+            // brands toUpdate2 = db.brands.FirstOrDefault(b => b.brand_id == selected.Id);
+
+            if (toUpdate == null
+                || int.TryParse(ProductPrice, out int productPrice) == false
+                || int.TryParse(ProductQuantity, out int productQuantity) == false)
+            {
+                return null;
+            }
+
+            toUpdate.product_name = ProductName;
+            toUpdate.product_price = productPrice;
+            toUpdate.product_quantity = productQuantity;
+
+            db.SaveChanges();
+
+            return toUpdate;
+        }
     }
 
     public class products_displayed
     {
         public int Id { get; set; }
         public string ProductName { get; set; }
-        // int?
         public int? BrandId { get; set; }
         public int? CategoryId { get; set; }
         public int ProductPrice { get; set; }
