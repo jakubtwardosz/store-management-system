00efa3364b401fc92ff4856a31101a033fe91370
Jakub Twardosz <jtwardosz13@gmail.com>
2021-02-26 23:21:29 +0100

    Customer Service completed

--- store-management-system-final/Customers.xaml.cs
+++ store-management-system-final/Customers.xaml.cs
@@ -30,27 +30,85 @@ namespace store_management_system_final
 
         private void AddCustomer(object sender, RoutedEventArgs e)
         {
+            if (CustomersService.TryAddCustomer(FirstName: CustomerFirstName.Text, LastName: CustomerLastName.Text, Email: CustomerEmail.Text, Street: CustomerStreet.Text, ZipCode: CustomerZipCode.Text, City: CustomerCity.Text, Phone: CustomerPhone.Text))
+            {
+                MessageBox.Show("Added!");
+            }
+            else
+            {
+                MessageBox.Show("Customer not added!");
+            }
+
 
         }
 
         private void customersGridDataSelectionChanged(object s, SelectionChangedEventArgs e)
         {
 
+            CustomersService.selected = GetSelectedDisplay(e);
+
+            var selected = CustomersService.selected;
+
+            CustomerFirstName.Text = CustomersService.selected?.FirstName;
+            CustomerLastName.Text = CustomersService.selected?.LastName;
+            CustomerEmail.Text = CustomersService.selected?.Email;
+            CustomerStreet.Text = CustomersService.selected?.Street;
+            CustomerZipCode.Text = CustomersService.selected?.ZipCode;
+            CustomerCity.Text = CustomersService.selected?.City;
+            CustomerPhone.Text = CustomersService.selected?.Phone.ToString();
         }
+
+        public static customers_displayed GetSelectedDisplay(SelectionChangedEventArgs e)
+        {
+            return e.AddedItems.Count == 0
+                ? null
+                : e.AddedItems[0] as customers_displayed;
+        }
+
+
 
         private void ReadCustomer(object sender, RoutedEventArgs e)
         {
+            CustomersDataGrid.ItemsSource = CustomersService.GetCustomersToDisplay();
+            MessageBox.Show("Loaded!");
 
         }
 
         private void UpdateCustomer(object sender, RoutedEventArgs e)
         {
+            if (CustomersService.selected == null)
+            {
+                NotSelectedMessage();
+                return;
+            }
 
+            var result = CustomersService.UpdateCustomer(FirstName: CustomerFirstName.Text, LastName: CustomerLastName.Text, Email: CustomerEmail.Text, Street: CustomerStreet.Text, ZipCode: CustomerZipCode.Text, City: CustomerCity.Text, Phone: CustomerPhone.Text);
+            if (result == null)
+            {
+                MessageBox.Show("Fail to update!");
+            }
+            else
+            {
+                MessageBox.Show("Updated!");
+            }
         }
 
         private void DeleteCustomer(object sender, RoutedEventArgs e)
         {
 
+            if (CustomersService.selected == null)
+            {
+                NotSelectedMessage();
+                return;
+            }
+
+            customers DeletedOrder = CustomersService.DeleteSelectedCustomer();
+            MessageBox.Show($"Deleted! {DeletedOrder?.customer_id}");
+        }
+
+        private static void NotSelectedMessage()
+        {
+            MessageBox.Show("You must select something!");
         }
     }
 }
--- store-management-system-final/CustomersService.cs
+++ store-management-system-final/CustomersService.cs
@@ -15,21 +15,117 @@ namespace store_management_system_final
             StoreDBEntities db = new StoreDBEntities();
 
             var customers = from c in db.customers
-                         select new customers_displayed
-                         {
-                             Id = c.customer_id,
-                             FirstName = c.first_name,
-                             LastName = c.last_name,
-                             Email = c.email,
-                             Street = c.street,
-                             ZipCode = c.zip_code,
-                             City = c.city,
-                             Phone = c.phone
-                         };
+                            select new customers_displayed
+                            {
+                                Id = c.customer_id,
+                                FirstName = c.first_name,
+                                LastName = c.last_name,
+                                Email = c.email,
+                                Street = c.street,
+                                ZipCode = c.zip_code,
+                                City = c.city,
+                                Phone = c.phone
+                            };
 
             return customers.ToList();
         }
+
+        public bool TryAddCustomer(string FirstName, string LastName, string Email, string Street, string ZipCode, string City, string Phone)
+        {
+            StoreDBEntities db = new StoreDBEntities();
+
+            if (int.TryParse(Phone, out int phone) 
+                && db.customers.All(customers => customers.email != Email)
+                && Phone.Length == 9 )
+            {
+                customers customersObject =
+                new customers()
+                {
+                    first_name = FirstName,
+                    last_name = LastName,
+                    email = Email,
+                    street = Street,
+                    zip_code = ZipCode,
+                    city = City,
+                    phone = phone
+                };
+
+                db.customers.Add(customersObject);
+
+                db.SaveChanges();
+
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public customers UpdateCustomer(string FirstName, string LastName, string Email, string Street, string ZipCode, string City, string Phone)
+        {
+            StoreDBEntities db = new StoreDBEntities();
+
+            var customers = from c in db.customers
+                         where c.customer_id == selected.Id
+                         select c;
+
+            customers toUpdate = customers.FirstOrDefault();
+            // brands toUpdate2 = db.brands.FirstOrDefault(b => b.brand_id == selected.Id);
+
+            if (toUpdate == null || int.TryParse(Phone, out int phone) == false || ZipCode.Length > 6)
+            {
+                return null;
+            }
+
+            toUpdate.first_name = FirstName;
+            toUpdate.last_name = LastName;
+            toUpdate.email = Email;
+            toUpdate.street = Street;
+            toUpdate.zip_code = ZipCode;
+            toUpdate.city = City;
+            toUpdate.phone = phone;
+
+            db.SaveChanges();
+
+            return toUpdate;
+        }
+
+        public customers DeleteSelectedCustomer()
+        {
+            StoreDBEntities db = new StoreDBEntities();
+
+            // Pobierz z bazy order_items razem z orderem
+
+            var filteredCustomer = from c in db.customers.Include(nameof(customers.orders))
+                                 where c.customer_id == selected.Id
+                                 select c;
+
+
+            customers toDelete = filteredCustomer.FirstOrDefault();
+
+            if (toDelete == null)
+            {
+                return null;
+            }
+
+            db.customers.Remove(toDelete);
+
+            db.SaveChanges();
+
+            return toDelete;
+        }
+
+
+
+
     }
+
+
+
+
+
+
+
 
     public class customers_displayed
     {
@@ -44,3 +140,4 @@ namespace store_management_system_final
     }
 
 }
+
